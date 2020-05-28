54ea56987eb881557ee905fdaa0e824e151ba934
geof bayzoes <geofbayzoes@gmail.com>
2020-05-28 00:26:51 -0400

    Updates

---
+++ precache-manifest.514f0988e77ab23573ae19669c352b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19568dc218f82cbf7de44f3d08e92d3",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "de8273d8c91be40ed5de",
+    "url": "/site/static/css/main.f9492839.chunk.css"
+  },
+  {
+    "revision": "c40d48acd9b501fe2a94",
+    "url": "/site/static/js/2.c0a85c8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/site/static/js/2.c0a85c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8273d8c91be40ed5de",
+    "url": "/site/static/js/main.85583442.chunk.js"
+  },
+  {
+    "revision": "7db7b1b809ea1615b3b1",
+    "url": "/site/static/js/runtime-main.489c679d.js"
+  }
+]);
